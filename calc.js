8b50e5e72c65fee4981704014100ead1218f1680
Terra Taylor <terra.taylor@gmail.com>
2021-09-16 13:26:26 -0500

    init commit

---
+++ calc.js
@@ -0,0 +1,12 @@
+function add(x,y){
+    return x+y;
+}
+function subtract(x,y){
+    return x-y;
+}
+
+let c=10;
+let z=5;
+
+
+module.exports={add, subtract, c, z};
---
+++ main.js
@@ -0,0 +1,8 @@
+const calc = require('./calc');
+const Joke = require('awesome-dev-jokes');
+ 
+console.log(Joke.getRandomJoke());
+//console.log(calc);
+
+//console.log(add(9,8));
+//console.log(subtract(9,8));
